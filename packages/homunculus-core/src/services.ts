984c222e6a62b342d0ea7fd49bd1835c00942d94
gwigz <gwigz@users.noreply.github.com>
2025-06-03 14:07:53 +0100

    fix(core): cannot access before initialization

--- packages/homunculus-core/src/services.ts
+++ packages/homunculus-core/src/services.ts
@@ -1,8 +1,16 @@
+/** biome-ignore-all lint/suspicious/noAssignInExpressions: wip */
 import { Authenticator, Delegate, Deserializer } from "./network"
+
+let deserializer: Deserializer | undefined
+let delegate: Delegate | undefined
 
 // shared stateless services that can be reused across multiple clients
 export const services = {
 	authenticator: new Authenticator("homunculus", "0.0.0"),
-	deserializer: new Deserializer(),
-	delegate: new Delegate(),
+	get deserializer() {
+		return (deserializer ??= new Deserializer())
+	},
+	get delegate() {
+		return (delegate ??= new Delegate())
+	},
 }
